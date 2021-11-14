f8b17fcbed77f612b5f21ef15822dd23f9dcc7d2
jonatastonholo <rtjonatas@gmail.com>
2021-11-14 13:15:41 -0300

    - add flyway plugin - created docker compose - created database creation script used by docker compose
    Signed-off-by: jonatastonholo <rtjonatas@gmail.com>

--- src/main/java/dev/tonholo/chronosimplesapi/ChronoSimplesApiApplication.java
+++ src/main/java/dev/tonholo/chronosimplesapi/ChronoSimplesApiApplication.java
@@ -2,11 +2,17 @@ package dev.tonholo.chronosimplesapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class ChronoSimplesApiApplication {
 
     public static void main(String[] args) {
+//        ReactorDebugAgent.init();
+//        ReactorDebugAgent.processExistingClasses();
+
+        BlockHound.install();
+
         SpringApplication.run(ChronoSimplesApiApplication.class, args);
     }
 
