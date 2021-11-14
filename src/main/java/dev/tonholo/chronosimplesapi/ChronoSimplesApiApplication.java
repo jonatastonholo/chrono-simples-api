1101652662a75c536a95aec63ce4b2856dfeb8ce
jonatastonholo <rtjonatas@gmail.com>
2021-11-14 10:52:38 -0300

    initial commit
    Signed-off-by: jonatastonholo <rtjonatas@gmail.com>

---
+++ src/main/java/dev/tonholo/chronosimplesapi/ChronoSimplesApiApplication.java
@@ -0,0 +1,13 @@
+package dev.tonholo.chronosimplesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronoSimplesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChronoSimplesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/tonholo/chronosimplesapi/ChronoSimplesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tonholo.chronosimplesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronoSimplesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
