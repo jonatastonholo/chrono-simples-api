cf9fc186c8e518b0f37efff19489a6ebf4e8897a
jonatastonholo <rtjonatas@gmail.com>
2021-12-06 09:11:53 -0300

    - fixed local date responses

--- src/main/java/dev/tonholo/chronosimplesapi/web/dto/ReportGenerationResponse.java
+++ src/main/java/dev/tonholo/chronosimplesapi/web/dto/ReportGenerationResponse.java
@@ -1,15 +1,20 @@
 package dev.tonholo.chronosimplesapi.web.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Value;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import static dev.tonholo.chronosimplesapi.config.JsonConfig.DATE_TIME_FORMAT;
+
 @Value
 @Builder
 public class ReportGenerationResponse {
+    @JsonFormat(pattern=DATE_TIME_FORMAT)
     LocalDateTime periodBegin;
+    @JsonFormat(pattern=DATE_TIME_FORMAT)
     LocalDateTime periodEnd;
     BigDecimal last12MonthEarnings;
     BigDecimal periodEarnings;
