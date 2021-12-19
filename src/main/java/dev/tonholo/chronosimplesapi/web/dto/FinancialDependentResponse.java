ee6b52f3932c4b8012382ed14b659ad9deb4cdbf
jonatastonholo <rtjonatas@gmail.com>
2021-12-18 23:36:11 -0300

    - fixed dates formats on requests and responses
    Signed-off-by: jonatastonholo <rtjonatas@gmail.com>

--- src/main/java/dev/tonholo/chronosimplesapi/web/dto/FinancialDependentResponse.java
+++ src/main/java/dev/tonholo/chronosimplesapi/web/dto/FinancialDependentResponse.java
@@ -1,10 +1,14 @@
 package dev.tonholo.chronosimplesapi.web.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Value;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+
+import static dev.tonholo.chronosimplesapi.config.JsonConfig.DATE_FORMAT;
+import static dev.tonholo.chronosimplesapi.config.JsonConfig.DATE_TIME_FORMAT;
 
 @Value
 @Builder
@@ -12,8 +16,12 @@ public class FinancialDependentResponse {
     String id;
     String name;
     Boolean irrfDeduct;
+    @JsonFormat(pattern=DATE_FORMAT)
     LocalDate periodBegin;
+    @JsonFormat(pattern=DATE_FORMAT)
     LocalDate periodEnd;
+    @JsonFormat(pattern=DATE_TIME_FORMAT)
     LocalDateTime createdAt;
+    @JsonFormat(pattern=DATE_TIME_FORMAT)
     LocalDateTime updatedAt;
 }
--- src/main/java/dev/tonholo/chronosimplesapi/web/dto/StopwatchResponse.java
+++ src/main/java/dev/tonholo/chronosimplesapi/web/dto/StopwatchResponse.java
@@ -1,10 +1,13 @@
 package dev.tonholo.chronosimplesapi.web.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Value;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+
+import static dev.tonholo.chronosimplesapi.config.JsonConfig.DATE_TIME_FORMAT;
 
 @Value
 @Builder
@@ -13,6 +16,8 @@ public class StopwatchResponse {
     String projectId;
     String description;
     BigDecimal hourValue;
+    @JsonFormat(pattern=DATE_TIME_FORMAT)
     LocalDateTime begin;
+    @JsonFormat(pattern=DATE_TIME_FORMAT)
     LocalDateTime end;
 }
